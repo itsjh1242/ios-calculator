cc81cb6ade2975d447e7edf2e17848bf0868f9bf
itsjh <93984898+itsjh1242@users.noreply.github.com>
2024-06-22 19:44:48 +0900

    feat: 숫자, AC, +/- 클릭 시 이벤트 추가

--- src/App.js
+++ src/App.js
@@ -5,22 +5,74 @@ function App() {
 
   const pressNumber = ({ number }) => {
     const newValue = parseInt(value + number).toString();
+    if (newValue.length < 10) {
+      setValue(newValue);
+    }
+  };
+
+  // 초기화
+  const pressAC = () => {
+    setValue("0");
+  };
+
+  // 양수, 음수형 변경
+  const pressNegative = () => {
+    var newValue;
+    if (value === "0") {
+      newValue = "-0";
+    } else {
+      newValue = (parseInt(value) * -1).toString();
+    }
     setValue(newValue);
   };
 
+  // Circle Component
+  const Circle = ({ children, bg, f }) => {
+    const onClickHandler = () => {
+      switch (f) {
+        case "AC":
+          pressAC();
+          break;
+        case "+/-":
+          pressNegative();
+          break;
+        default:
+          pressNumber({ number: f });
+          break;
+      }
+    };
+    return (
+      <div
+        className={`w-14 h-14 rounded-full flex justify-center items-center ${f === 0 ? "col-span-2 w-full" : ""} ${bg}`}
+        onClick={() => {
+          onClickHandler();
+        }}
+      >
+        {children}
+      </div>
+    );
+  };
+
   return (
-    <main className="w-screen h-screen flex justify-center items-center">
-      <div className="w-[320px] h-[600px] bg-black p-4">
+    <main className="relative w-screen h-screen flex justify-center items-center">
+      <div className="absolute top-1/2 left-1/2 -translate-x-[345px] -translate-y-[345px] w-[700px] h-[500px]">
+        <img src="./iphone.png" alt="iphone" />
+      </div>
+      <div className="z-20 w-[325px] h-[660px] bg-black p-4 rounded-[50px]">
         <div className="w-full h-1/3 flex justify-end items-end p-5">
-          <p className="text-6xl text-white text-right">{value.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}</p>
+          <p
+            className={`text-white text-right ${value.length >= 9 ? "text-4xl" : value.length >= 8 ? "text-5xl" : value.length >= 7 ? "text-6xl" : "text-7xl"}`}
+          >
+            {value.replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}
+          </p>
         </div>
         <div className="w-full h-2/3 flex justify-center items-start">
           <div className="grid grid-cols-4 gap-4">
             {/* Row 1 */}
-            <Circle bg="bg-light-gray">
+            <Circle bg="bg-light-gray" f={"AC"}>
               <p className="text-2xl font-semibold">AC</p>
             </Circle>
-            <Circle bg="bg-light-gray">
+            <Circle bg="bg-light-gray" f={"+/-"}>
               <p className="text-2xl font-semibold">+/-</p>
             </Circle>
             <Circle bg="bg-light-gray">
@@ -30,53 +82,48 @@ function App() {
               <p className="text-3xl font-semibold text-white">÷</p>
             </Circle>
             {/* Row 2 */}
-            <Circle
-              bg="bg-deep-gray"
-              onClick={() => {
-                pressNumber({ number: "7" });
-              }}
-            >
+            <Circle bg="bg-deep-gray" f={7}>
               <p className="text-2xl font-semibold text-gray-200">7</p>
             </Circle>
-            <Circle bg="bg-deep-gray">
+            <Circle bg="bg-deep-gray" f={8}>
               <p className="text-2xl font-semibold text-gray-200">8</p>
             </Circle>
-            <Circle bg="bg-deep-gray">
+            <Circle bg="bg-deep-gray" f={9}>
               <p className="text-2xl font-semibold text-gray-200">9</p>
             </Circle>
             <Circle bg="bg-deep-orange">
               <p className="text-3xl font-semibold text-white">×</p>
             </Circle>
             {/* Row 3 */}
-            <Circle bg="bg-deep-gray">
+            <Circle bg="bg-deep-gray" f={4}>
               <p className="text-2xl font-semibold text-gray-200">4</p>
             </Circle>
-            <Circle bg="bg-deep-gray">
+            <Circle bg="bg-deep-gray" f={5}>
               <p className="text-2xl font-semibold text-gray-200">5</p>
             </Circle>
-            <Circle bg="bg-deep-gray">
+            <Circle bg="bg-deep-gray" f={6}>
               <p className="text-2xl font-semibold text-gray-200">6</p>
             </Circle>
             <Circle bg="bg-deep-orange">
               <p className="text-3xl font-semibold text-white">−</p>
             </Circle>
             {/* Row 4 */}
-            <Circle bg="bg-deep-gray">
+            <Circle bg="bg-deep-gray" f={1}>
               <p className="text-2xl font-semibold text-gray-200">1</p>
             </Circle>
-            <Circle bg="bg-deep-gray">
+            <Circle bg="bg-deep-gray" f={2}>
               <p className="text-2xl font-semibold text-gray-200">2</p>
             </Circle>
-            <Circle bg="bg-deep-gray">
+            <Circle bg="bg-deep-gray" f={3}>
               <p className="text-2xl font-semibold text-gray-200">3</p>
             </Circle>
             <Circle bg="bg-deep-orange">
               <p className="text-3xl font-semibold text-white">+</p>
             </Circle>
             {/* Row 5 */}
-            <div className={`w-full h-14 rounded-full flex justify-start items-center bg-deep-gray col-span-2 pl-5`}>
+            <Circle bg="bg-deep-gray" f={0}>
               <p className="text-3xl font-semibold text-gray-200">0</p>
-            </div>
+            </Circle>
             <Circle bg="bg-deep-gray">
               <p className="text-3xl font-semibold text-gray-200">.</p>
             </Circle>
@@ -89,14 +136,5 @@ function App() {
     </main>
   );
 }
-
-// Circle Component
-const Circle = ({ children, bg, onClick }) => {
-  return (
-    <div className={`w-14 h-14 rounded-full flex justify-center items-center ${bg}`} onClick={onClick}>
-      {children}
-    </div>
-  );
-};
 
 export default App;
